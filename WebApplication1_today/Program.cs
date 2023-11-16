157b6ad479498a5cb2defe63a025164529b67872
ajinkyaanwade <ajinkya.anwade512000@gmail.com>
2023-11-17 00:11:13 +0530

    done

--- WebApplication1_today/Program.cs
+++ WebApplication1_today/Program.cs
@@ -12,7 +12,7 @@ namespace WebApplication1_today
 
             // Add services to the container.
 
-            builder. Services.AddControllers();
+            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
